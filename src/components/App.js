a96683708c2a5a44af4e3d7179faeff918fb2f78
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-09-04 02:01:00 -0700

    propTypes within context-trees
    declare the structure of the context object being passed to your component’s descendants.
    getChildContext: prototype method that returns the context object to pass down the component’s hierarchy. Every time the state changes or the component receives new props this method will be called.

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ContextType = {
+    //Enables critical path CSS rendering
+    //https://github.com/kriasoft/isomorphic-style-loader
+    insertCss: PropTypes.func.isRequired,
+    //Universal HTTP client
+    fetch: PropTypes.func.isRequired,
+
+};
+
+/**
+ * The Top level react component setting context (global variables)
+ * that can be accessed from all the child components.
+ *
+ *
+ */
+
+class App extends React.PureComponent {
+    static propTypes = {
+	context: PropTypes.shape(ContextType).isRequired,
+	children: PropTypes.element.isRequired,
+
+    };
+
+    static childContextTypes = ContextType;
+
+    getChildContext() {
+	return this.props.context;
+    }
+
+    render() {
+
+	return React.Children.only(this.props.children);
+    }
+}
